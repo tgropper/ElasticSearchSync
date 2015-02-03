77dc61caabfbc629beefe593a7fc2e91eed77753
tgropper <tgropper@originsoftware.com.ar>
2015-02-03 16:33:53 -0300

    ES conn + bulk serializer

--- ElasticSearchSync/Helpers/DataReaderExtensions.cs
+++ ElasticSearchSync/Helpers/DataReaderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 
@@ -6,28 +7,53 @@ namespace ElasticSearchSync.Helpers
 {
     public static class DataReaderExtensions
     {
-        public static IEnumerable<Dictionary<string, object>> Serialize(this SqlDataReader reader)
+        public static Dictionary<object, Dictionary<string, object>> Serialize(this SqlDataReader reader)
         {
-            var results = new List<Dictionary<string, object>>();
+            var results = new Dictionary<object, Dictionary<string, object>>();
             var cols = new List<string>();
             for (var i = 0; i < reader.FieldCount; i++)
                 cols.Add(reader.GetName(i));
 
             while (reader.Read())
             {
-                var r = SerializeRow(cols, reader, results);
-                results.Add(r);
+                var r = SerializeRow(cols, reader);
+                results.Add(r.Values.First(), r);
             }
 
             return results;
         }
 
-        private static Dictionary<string, object> SerializeRow(IEnumerable<string> cols, SqlDataReader reader, IEnumerable<Dictionary<string, object>> results)
+        public static Dictionary<object, Dictionary<string, object>> SerializeArray(this SqlDataReader reader, Dictionary<object, Dictionary<string, object>> results)
+        {
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+                cols.Add(reader.GetName(i));
+
+            while (reader.Read())
+            {
+                var r = SerializeRow(cols, reader);
+                if (!results.ContainsKey(r.Values.First()))
+                    throw new Exception();
+
+                var _object = results[r.Values.First()];
+                r = r.Skip(1).ToDictionary(x => x.Key, x => x.Value);
+                var elem = ((Dictionary<string, object>)((Dictionary<string, object>)_object)[r.Keys.First()]);
+                var arrayElemKey = ((Dictionary<string, object>)r.Values.First()).Keys.First();
+                if (!elem.ContainsKey(arrayElemKey))
+                    elem.Add(arrayElemKey, new List<object>());
+
+                ((List<object>)elem[arrayElemKey]).Add(r[r.Keys.First()]);
+            }
+
+            return results;
+        }
+
+        private static Dictionary<string, object> SerializeRow(IEnumerable<string> cols, SqlDataReader reader)
         {
             var result = new Dictionary<string, object>();
             foreach (var col in cols)
             {
-                SerializeObject(col, col, reader, result, results);
+                SerializeObject(col, col, reader, result);
             }
             return result;
         }
@@ -36,8 +62,7 @@ namespace ElasticSearchSync.Helpers
             string col,
             string fullColName,
             SqlDataReader reader,
-            Dictionary<string, object> result,
-            IEnumerable<Dictionary<string, object>> results)
+            Dictionary<string, object> result)
         {
             var objIndex = col.IndexOf('.');
             if (objIndex != -1)
@@ -50,14 +75,12 @@ namespace ElasticSearchSync.Helpers
                     col.Substring(objIndex + 1),
                     fullColName,
                     reader,
-                    (Dictionary<string, object>)result[objName],
-                    results.Where(x => x.ContainsKey(objName)));
+                    (Dictionary<string, object>)result[objName]);
                 return result;
             }
             else
             {
                 var val = reader[fullColName];
-
                 result[col] = val;
                 return result;
             }
--- ElasticSearchSync/Program.cs
+++ ElasticSearchSync/Program.cs
@@ -1,11 +1,11 @@
-﻿using ElasticSearchSync.Helpers;
+﻿using Elasticsearch.Net;
+using Elasticsearch.Net.Connection;
+using ElasticSearchSync.Helpers;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElasticSearchSync
 {
@@ -20,52 +20,56 @@ namespace ElasticSearchSync
             try
             {
                 conn.Open();
-//                SqlCommand cmd = new SqlCommand(@"
-//                    SELECT TOP 10 rat.ID AS 'ID', 
-//			  net.ID AS 'Network.ID', 
-//			  net.Description AS 'Network.Description',
-//			  country.Description AS 'Network.Country.Description',
-//			  country.IsoCode AS 'Network.Country.IsoCode',
-//              region.Description AS 'Network.Country.Region.Description',
-//			  rat.RatingPerc AS 'RatingPerc'
-//FROM dbo.ChannelRating rat 
-//JOIN dbo.ChannelNetwork net ON rat.NetworkID = net.ID 
-//JOIN dbo.Country country ON net.CountryID = country.ID
-//JOIN dbo.Region region ON country.RegionID = region.ID
-//WHERE rat.RatingPerc <> 0"
-//                                    , conn);
-
-//                SqlCommand cmd = new SqlCommand(@"
-//                    SELECT TOP 10 net.ID AS '_id', net.Description AS 'Description', rat.RatingPerc AS 'Rating.Perc'
-//FROM dbo.ChannelNetwork net
-//JOIN dbo.ChannelRating rat ON rat.NetworkID = net.ID
-//WHERE rat.RatingPerc <> 0"
-//                    , conn);
 
                 SqlCommand cmd = new SqlCommand(@"
-                    SELECT TOP 10 mg.ID AS '_id', 
-			  mg.Description AS 'Description', 
-			  nw.Description AS 'Network.Description', 
-			  rt.RatingPerc AS 'Rating.Perc'
+                    SELECT TOP 3 mg.ID AS '_id',
+              mg.ID AS 'MediaGroup._id', 
+			  mg.Description AS 'MediaGroup.Description'
 FROM dbo.ChannelMediaGroup mg
-JOIN dbo.ChannelNetwork nw ON mg.ID = nw.MediaGroupID
-JOIN dbo.ChannelRating rt ON mg.ID = rt.MediaGroupID
-WHERE rt.RatingPerc <> 0"
+WHERE mg.CountryID IS NULL"
                     , conn);
 
                 rdr = cmd.ExecuteReader();
                 var data = rdr.Serialize();
-                var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                rdr.Close();
+                
+                cmd = new SqlCommand(String.Format(@"
+                    SELECT TOP 6 mg.ID AS '_id',
+			  nw.Description AS 'MediaGroup.Network.Description',
+			  ct.IsoCode AS 'MediaGroup.Network.Country.IsoCode'
+            FROM dbo.ChannelMediaGroup mg
+            JOIN dbo.ChannelNetwork nw ON mg.ID = nw.MediaGroupID
+            JOIN dbo.Country ct ON ct.ID = nw.CountryID
+            WHERE mg.ID IN ({0}) AND mg.CountryID IS NULL", String.Join(",", data.Keys))
+                     , conn);
+                var arrayrdr = cmd.ExecuteReader();
 
-                Console.WriteLine(json);
+                data = arrayrdr.SerializeArray(data);
+                arrayrdr.Close();
+
+                string bulk = "";
+                foreach (var bulkData in data)
+                {
+                    bulk = bulk + String.Format("{0}\n{1}\n",
+                        JsonConvert.SerializeObject(new { index = new { _index = "ratings", _type = "mediaGroup", _id = bulkData.Key } }, Formatting.None),
+                        JsonConvert.SerializeObject(bulkData.Value, Formatting.None));
+                }
+
+                Console.WriteLine(bulk);
+
+                var node = new Uri("http://localhost:9200"); //config
+                var config = new ConnectionConfiguration(node).UsePrettyResponses();
+                var client = new ElasticsearchClient(config);
+
+                var response = client.Bulk(bulk);
+
+                Console.WriteLine(response.Success);
+                Console.WriteLine(response.HttpStatusCode);
 
                 Console.ReadLine();
             }
             finally
             {
-                if (rdr != null)
-                    rdr.Close();
-
                 if (conn != null)
                     conn.Close();
             }
